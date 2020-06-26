ab8a41c53222c24f5c7103bedbd5963ad2f9abae
Compute with lucifer <computewithlucifer@gmail.com>
2020-06-26 17:58:20 +0530

    added requirements.txt,added home app,created post sytem,completed html templates folders

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('hello')
+    return render(request, 'blog/index.html')
+
+
+def blogPost(request,slug):
+    return HttpResponse(f'this is blog {slug}')
+    # return render(request, 'blog/index.html')
