a78bb48bedc2d9f221cc2590769156cf5b7dc973
Compute with lucifer <computewithlucifer@gmail.com>
2020-06-27 09:30:05 +0530

    created contactus page and backend

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    s_no = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    desc = models.TextField(default="")
+    res_time=models.DateTimeField(auto_now_add=True,blank=True)
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
 # Create your views here.
 
 
 def index(request):
-    return render(request,'home/index.html')
+    return render(request, 'home/index.html')
 
 
 def about(request):
-    return render(request,'home/about.html')
+    return render(request, 'home/about.html')
 
 
 def contact(request):
-    return render(request,'home/contactus.html')
+    res = False
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        msg = request.POST.get('desc', '')
+        contactsave = Contact(name=name,email=email,phone=phone,desc=msg)
+        contactsave.save()
+        res  =True
+        return render(request, 'home/contactus.html',{"res":res})
+    return render(request, 'home/contactus.html')
