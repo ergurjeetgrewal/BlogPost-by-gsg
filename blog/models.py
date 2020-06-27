5a4e07e31e96fc45e929d33f0aff5628bec28f5e
Compute with lucifer <computewithlucifer@gmail.com>
2020-06-27 11:54:23 +0530

    created search page ,blogs displayed,error slug managed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    s_no = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=220)
+    content=models.TextField()
+    author = models.CharField(max_length=13)
+    timestamp = models.DateTimeField(blank=True)
+    slug = models.CharField(max_length=130,default='')
+    def __str__(self):
+        return self.title +' '+ self.author
+
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from blog.models import Post
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    allposts=Post.objects.all()
+    context = {'allposts':allposts}
+    return render(request, 'blog/index.html',context)
 
 
 def blogPost(request,slug):
-    # return HttpResponse(f'this is blog {slug}')
-    return render(request, 'blog/blogpost.html',{'params':slug})
+    post=Post.objects.filter(slug=slug).first()
+    if post != None:
+        context = {'post':post}
+    else:
+        return HttpResponse('error')
+    print(context)
+    return render(request, 'blog/blogpost.html',context)
