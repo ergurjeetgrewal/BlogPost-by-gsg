c12ba2c0fedf7595aba0b17470c11ea1a28ce152
Gurjeet Singh Grewal <gurgrewal143@gmail.com>
2021-11-06 10:37:03 +0530

    files names and bugs fixes

--- blog/models.py
+++ blog/models.py
@@ -8,21 +8,22 @@ class Post(models.Model):
     s_no = models.AutoField(primary_key=True)
     title = models.CharField(max_length=220)
     head_title = models.CharField(max_length=220)
-    content=models.TextField()
+    content = models.TextField()
     author = models.CharField(max_length=13)
     timestamp = models.DateTimeField(blank=True)
-    slug = models.CharField(max_length=130,default='')
+    slug = models.CharField(max_length=130, default='')
+
     def __str__(self):
-        return self.title +' '+ self.author
+        return self.title + ' ' + self.author
+
 
 class Blogcomment(models.Model):
-    sno=models.AutoField(primary_key=True)
+    sno = models.AutoField(primary_key=True)
     comment = models.TextField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
-    parent = models.ForeignKey('self',on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
     timestamp = models.DateTimeField(default=now)
 
     def __str__(self):
         return self.comment[0:15]+'... By: '+str(self.user)
-    
